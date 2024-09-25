ba614f2a51ab42583bfc8adb818c823f6cc234d1
pollocandente <a.23308051281211@cbtis128.edu.mx>
2024-09-24 19:52:26 -0600

    Add files via upload

---
+++ adivinar numero.py
@@ -0,0 +1,7 @@
+print(" ")#define una linea en blanco
+print("Roman De la Cruz Leonardo Antonio, adivina el numero")
+print(" ")#define una linea en blanco
+numero1 = int(input("Introduce el primero numero: "))#solicita que ingresemos el valor
+print(" ")#define una linea en blanco
+print(f"el numero ingresado es:{numero1}")#print imprime el numero ingresado
+print(" ")#define una linea en blanco
