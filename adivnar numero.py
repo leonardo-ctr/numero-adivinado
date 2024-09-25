70be8c730cec51fc32b0eecd95880edb23fc0c4e
pollocandente <a.23308051281211@cbtis128.edu.mx>
2024-09-24 20:27:51 -0600

    Add files via upload

---
+++ adivnar numero.py
@@ -0,0 +1,15 @@
+print("Roman De la Cruz Leonardo Antonio, numero")
+A = 3
+print(" ")#define una linea en blanco
+#solicita que ingrese el valor
+print(" ")#define una linea en blanco
+numero = int(input("Introduce  numero: "))
+#verifica que el valor sea el de la variable
+if (A == numero):
+    #imprime si es correcto
+    print(" el numero es correcto")
+    print(" ")#define una linea en blanco
+else:
+    #imprime si es incorrecto
+    print(" el numero es incorrecto")
+    print(" ")#define una linea en blanco
